89d53e6b0d74bc567824a7ec2bb7c2d08c9bfd2f
neil <pc@byneil.com>
2016-09-21 21:52:58 +0800

    test local address

--- letest.sh
+++ letest.sh
@@ -410,8 +410,7 @@ le_test_standandalone_renew() {
   if [ "$lp" ] ; then
     __fail "80 port is not released: $lp"
     return 1
-  fi
-  
+  fi  
   
 
 }
@@ -441,6 +440,61 @@ le_test_standandalone_renew_v2() {
   ca="$certdir/ca.cer"
   full="$certdir/full.cer"
   _assertcmd "$lehome/$PROJECT_ENTRY --issue -d $TestingDomain --standalone --certpath '$cert' --keypath '$key'  --capath '$ca'  --reloadcmd 'echo this is reload'  --fullchainpath  '$full'" ||  return
+  
+  _assertfileequals "$lehome/$TestingDomain/$TestingDomain.cer" "$cert" ||  return
+  _assertfileequals "$lehome/$TestingDomain/$TestingDomain.key" "$key" ||  return
+  _assertfileequals "$lehome/$TestingDomain/ca.cer" "$ca" ||  return
+  _assertfileequals "$lehome/$TestingDomain/fullchain.cer" "$full" ||  return
+  
+  rm -rf "$certdir"
+  mkdir -p "$certdir"
+  
+  sleep 5
+  _assertcmd "$lehome/$PROJECT_ENTRY --renew -d $TestingDomain --force" ||  return
+  
+  _assertcert "$lehome/$TestingDomain/$TestingDomain.cer" "$TestingDomain" "$CA" || return
+  _assertcert "$lehome/$TestingDomain/ca.cer" "$CA" || return
+  
+  _assertfileequals "$lehome/$TestingDomain/$TestingDomain.cer" "$cert" ||  return
+  _assertfileequals "$lehome/$TestingDomain/$TestingDomain.key" "$key" ||  return
+  _assertfileequals "$lehome/$TestingDomain/ca.cer" "$ca" ||  return
+  _assertfileequals "$lehome/$TestingDomain/fullchain.cer" "$full" ||  return
+  
+  rm -rf "$certdir"
+  
+  lp=`_ss | grep ':80 '`
+  if [ "$lp" ] ; then
+    __fail "80 port is not released: $lp"
+    return 1
+  fi
+
+}
+
+
+#
+le_test_standandalone_renew_localaddress_v2() {
+  lehome="$Default_Home"
+
+  lp=`_ss | grep ':80 '`
+  if [ "$lp" ] ; then
+    __fail "80 port is already used."
+    return 1
+  fi
+  
+  if [ -z "$TestingDomain" ] ; then
+    __fail "Please define TestingDomain and try again."
+    return 1
+  fi
+
+  rm -rf "$lehome/$TestingDomain"
+  
+  certdir="$(pwd)/certs"
+  mkdir -p "$certdir"
+  cert="$certdir/domain.cer"
+  key="$certdir/domain.key"
+  ca="$certdir/ca.cer"
+  full="$certdir/full.cer"
+  _assertcmd "$lehome/$PROJECT_ENTRY --issue -d $TestingDomain --standalone --local-address 0.0.0.0 --certpath '$cert' --keypath '$key'  --capath '$ca'  --reloadcmd 'echo this is reload'  --fullchainpath  '$full'" ||  return
   
   _assertfileequals "$lehome/$TestingDomain/$TestingDomain.cer" "$cert" ||  return
   _assertfileequals "$lehome/$TestingDomain/$TestingDomain.key" "$key" ||  return
